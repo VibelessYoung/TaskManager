9154214793ec714b325c14702600138057d8a7d2
Amir <vibelessyoung@gmail.com>
2026-06-05 17:23:03 +0330

    header created

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,7 +1,25 @@
 import React from "react";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="bg-gray-950 text-2xl flex justify-center font-bold text-white p-6 gap-4 items-center">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="size-6 animate-pulse"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M2.25 15a4.5 4.5 0 0 0 4.5 4.5H18a3.75 3.75 0 0 0 1.332-7.257 3 3 0 0 0-3.758-3.848 5.25 5.25 0 0 0-10.233 2.33A4.502 4.502 0 0 0 2.25 15Z"
+        />
+      </svg>
+      Manage Your Tasks
+    </div>
+  );
 }
 
 export default Header;
--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,12 @@
 import "@/app/globals.css";
+import Header from "./components/Header";
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="">
-      <body className="">{children}</body>
+      <body className="">
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
