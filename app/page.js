8d9f4ce030a5879c5456d2130c224f0e64ab965d
Amir <vibelessyoung@gmail.com>
2026-06-10 23:44:40 +0330

    add changes to home page

--- app/page.js
+++ app/page.js
@@ -2,8 +2,8 @@ import Tasks from "./components/Tasks";
 
 export default function Home() {
   return (
-    <div className="bg-gray-900 w-full">
-      <Tasks />
+    <div className="bg-gray-900 w-full flex justify-center items-center">
+      <h1 className="text-white text-6xl animate-pulse">HELLO USER</h1>
     </div>
   );
 }
