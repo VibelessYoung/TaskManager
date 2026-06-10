47b3cbf783b525e44f2111199c5e4ea245930de3
Amir <vibelessyoung@gmail.com>
2026-06-10 16:08:58 +0330

    search page created

--- app/search/page.js
+++ app/search/page.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 function page() {
-  return <div>page</div>;
+  return (
+    <div className="flex flex-col bg-gray-900 w-full items-center p-5">
+      <h1 className="text-3xl text-white font-bold">Search</h1>
+      <input
+        type="search"
+        className="border-2 border-white rounded-sm mt-10 w-2xl p-1.5 text-white"
+      />
+    </div>
+  );
 }
 
 export default page;
